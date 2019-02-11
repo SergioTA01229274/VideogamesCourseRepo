8c039354947f3cb897f5fa4697d9bba098dd7492
Sergio Ivan Tostado Nieto <30873981+SergioTA01229274@users.noreply.github.com>
2019-02-10 23:28:48 -0600

    Adding Assets directory updates

---
+++ TankFight/Assets/Assets/Scripts/Player1.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1 : MonoBehaviour
+{
+
+
+    public int speed;
+    private bool isMyTurn;
+    public GameObject projectile, pivot, adversary;
+
+    void Start()
+    {
+        isMyTurn = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (this.getIsMyTurn() == true)
+        {
+            float horizontalAxis = Input.GetAxis("Horizontal");
+            transform.Translate(speed * horizontalAxis * Time.deltaTime, 0, 0, Space.World);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.pivot.transform.Rotate(Vector3.left * 100f * Time.deltaTime);
+        }else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.pivot.transform.Rotate(Vector3.left * -100f * Time.deltaTime);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(projectile);
+        }
+
+    }
+
+    public void setIsMyTurn()
+    {
+        isMyTurn = !isMyTurn;
+    }
+    public bool getIsMyTurn()
+    {
+        return isMyTurn;
+    }
+}
---
+++ TankFight/Assets/Assets/Scripts/Player2.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2 : MonoBehaviour
+{
+    public int speed;
+    public bool isMyTurn;
+
+
+    void Start()
+    {
+        isMyTurn = false;
+    }
+
+
+    void Update()
+    {
+
+        if (this.getIsMyTurn() == true)
+        {
+            float horizontalAxis = Input.GetAxis("Horizontal");
+            transform.Translate(speed * horizontalAxis * Time.deltaTime, 0, 0, Space.World);
+        }
+
+
+    }
+
+    public void setIsMyTurn()
+    {
+        isMyTurn = !isMyTurn;
+    }
+    public bool getIsMyTurn()
+    {
+        return isMyTurn;
+    }
+}
