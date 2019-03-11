5221e9646a2d355495bad924f1a6f36b60c32aea
SergioTA01229274 <30873981+SergioTA01229274@users.noreply.github.com>
2019-03-10 18:40:41 -0600

    Class-05-03-2019 project
    Working with empty objects

---
+++ Class-05-03-2019/Assets/Character.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public WayPoint[] path;
+    public float range;
+    private int current;
+
+
+    void Start()
+    {
+        current = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(path[current].transform);
+        transform.Translate(transform.forward * Time.deltaTime * 2, Space.World);
+    }
+
+    //We need to checj distance on a regular basis with the least amount of overhead
+    //Coroutine!!!
+
+    IEnumerator CheckDistance(){
+
+        
+        while (true)
+        {
+            float d = Vector3.Distance(transform.position, path[current].transform.position);
+
+            if (d < range)
+            {
+                current++;
+                current %= path.Length; 
+            }
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+}
---
+++ Class-05-03-2019/Assets/WayPoint.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPoint : MonoBehaviour
+{
+    public WayPoint[] neighbors;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(transform.position, 1);
+
+        Gizmos.color = Color.green;
+        foreach (WayPoint current in neighbors){
+            Gizmos.DrawLine(transform.position, current.transform.position);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position, 1);
+    }
+}
